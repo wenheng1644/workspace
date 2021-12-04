70ac5f027f25f2fb8adfa8785846719cbd9c95e3
wenheng1644 <wenheng1644@gmail.com>
2021-12-04 22:43:04 +0800

    小小修改

--- CppExcise/Cpp_Primer/Cpp_bitset/ex_bitset.cpp
+++ CppExcise/Cpp_Primer/Cpp_bitset/ex_bitset.cpp
@@ -9,13 +9,13 @@ void ex_bitset()
     auto res = b.to_ullong();
     cout << "to_ullong: " << res << endl; 
     cout << "b[0]: " << b[0] << endl;
-    b[0] = 111;
+    b[0] = 1;
     cout << "reset b[0]: " << b[0] << endl;
     cout << "the \'1\' have: " << b.count() << endl;  
     cout << "flip(): " << b.flip() << endl;
 }
 
-int main(void)
+int main()
 {
     ex_bitset();
     return 0;
