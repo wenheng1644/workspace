65c929db4c829b7079b49c07827c747d9ab9e7fc
wenheng1644 <wenheng1644@gmail.com>
2021-12-26 00:53:14 +0800

    抽卡模拟器app初次提交

---
+++ CppExcise/Cpp_Primer/DrawCardApp/source/server/drawCard/draw.h
@@ -0,0 +1,31 @@
+#ifndef _DRAW_
+#define _DRAW_
+
+#include <vector>
+#include <string>
+
+#include "drawcard_Data.h"
+#include "../CardPool/cardpool.h"
+
+using namespace std;
+
+
+
+//抽卡类
+class draw{
+public:
+
+    draw();
+    ~draw();
+
+    //单抽
+    drawcard_data singleGet();
+
+    //十连抽
+    vector<drawcard_data> tenGet();
+};
+
+
+
+
+#endif
