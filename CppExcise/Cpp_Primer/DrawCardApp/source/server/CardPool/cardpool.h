7aea262c250fd31f779f609121aef407f91061f6
wenheng1644 <wenheng1644@gmail.com>
2021-12-26 18:36:18 +0800

    抽卡模拟器后端逻辑框架大致完成

--- CppExcise/Cpp_Primer/DrawCardApp/source/server/CardPool/cardpool.h
+++ CppExcise/Cpp_Primer/DrawCardApp/source/server/CardPool/cardpool.h
@@ -15,20 +15,34 @@ class draw;
 class cardpool{
     friend draw;
 public:
-    cardpool(string abs_file,string pro_file);
-    ~cardpool();
-
-    bool loadConfig(string abs_file);
+    
+    static cardpool* GetCardPool()
+    {
+        if(!m_cardpool_ptr)
+        {
+            m_cardpool_ptr = new cardpool();
+        }
+        
+        return m_cardpool_ptr;
+    }
 
     //是否已加载卡池
     bool isLoadCardPool() { return !m_cardpool.empty(); }
-    
-    //获取对应稀有度卡池 
-    //rare默认值为0，则是全部卡池
-    map<int,drawcard_data> getCardPool(int rare = 0);
 
+    //根据需要显示相应的内容
+    //num = 0: 显示所有卡池信息
+    //num = 1: 显示稀有度卡池信息
+    //num = 2: 显示稀有度概率信息
+    void showCardInfo(int num = 0);
+
+    //抽卡，默认是单抽
+    vector<drawcard_data> getRandom_drawcard(int cnt = 1);
 private:
-    map<int,drawcard_data> m_cardpool;                      //所有卡池
+    //静态指针
+    static cardpool* m_cardpool_ptr;
+
+    //所有卡池
+    map<int,drawcard_data> m_cardpool;                      
 
     //不同稀有度的卡池  稀有度作为key，对应卡池为value
     map<int,map<int,drawcard_data>> m_quality_cardpool;
@@ -36,11 +50,16 @@ private:
     //不同稀有度所对应的概率
     vector<int> m_RareProPool;
 
+    cardpool();
+
     //切割一行字符串数据
     drawcard_data get_drawcard(string line);
 
     //切割一行字符串数据获得稀有度概率
     rarepro_data get_rarePro(string line);
+
+    //加载卡池配置文件
+    bool loadConfig(string abs_file);
 
     //加载对应不同稀有度的卡池
     void load_configquality_drawcard_pool(drawcard_data& data);
@@ -50,7 +69,19 @@ private:
 
     //通过随机数返回对应稀有度
     int getRareByRandom(size_t rand_num);
-};
 
+    //获取对应稀有度卡池
+    //rand_num随机数 
+    map<int,drawcard_data> getCardPool(size_t rand_num);
+
+    //打印卡池
+    void print_Card(map<int,drawcard_data>& mapInfo);
+    void show_RareCardPool();
+    void show_AllCardPool();
+    void show_RarePro();
+
+    //检查数据的合法性
+    bool checkData(drawcard_data& data);
+};
 
 #endif
--- CppExcise/Cpp_Primer/DrawCardApp/source/server/drawCard/drawcard_Data.h
+++ CppExcise/Cpp_Primer/DrawCardApp/source/server/drawCard/drawcard_Data.h
@@ -10,21 +10,25 @@ using namespace std;
 //卡的数据结构
 struct drawcard_data
 {
-    size_t     m_id;    //卡id
+    int     m_id;    //卡id
     string  m_name;     //卡名
-    size_t  m_quality;  //卡品质（1-5）
+    int  m_quality;  //卡品质（1-5）
     int  m_pro;      //卡概率
 
-    drawcard_data() : m_id(-1),m_name(""),m_quality(-1),m_pro(-1) {}
+    drawcard_data() : m_id(-1),m_name(string()),m_quality(-1),m_pro(-1) {}
 };
 
 //稀有度的数据结构
 struct rarepro_data
 {
-    size_t m_quality;
+    int m_quality;
     int    m_pro; 
 
     rarepro_data() : m_quality(0),m_pro(0) {}
 };
+
+//配置文件路径
+#define CARDPOOL_CONFIG  "drawCardInfo.csv"
+#define RARE_CONFIG "rare_quality_pro.csv"
 
 #endif
