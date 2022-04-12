1965228dc6b9ef69b2bc158a70a0f45877e39a3f
wenheng1644 <wenheng1644@gmail.com>
2022-04-12 22:07:19 +0800

    按教程方式写聊天室服务端

--- CppExcise/boostExcise/boost_asio/chat_project/server/chatSession.cpp
+++ CppExcise/boostExcise/boost_asio/chat_project/server/chatSession.cpp
@@ -7,33 +7,42 @@ void chatSession::start()
 {
     m_pRoom.join(shared_from_this());
 
-    boost::asio::async_read(m_sock, buffer(m_chatMsg.getData(), m_chatMsg.getLen()), boost::bind(&chatSession::handler_read, shared_from_this(), boost::asio::placeholders::error));
+    boost::asio::async_read(m_sock, buffer(m_readingChatMsg.getData(), HEADER_LEN), boost::bind(&chatSession::handler_readhead, shared_from_this(), boost::asio::placeholders::error));
 }
 
-void chatSession::handler_read(boost::system::error_code ec)
+void chatSession::handler_readhead(const error_code_type &ec)
 {
     if(ec)
     {
         std::cerr << "数据读取错误" << std::endl;
         m_pRoom.leave(shared_from_this());
-    }
-}
-
-void chatSession::handler_write(boost::system::error_code ec)
-{
-    if(ec)
-    {
-        std::cerr << "数据写入错误" << std::endl;
         return;
     }
 
-    if(!m_wirteMsg.empty())
+    if(!m_readingChatMsg.decode())
     {
-        boost::asio::async_write(m_sock, buffer(m_wirteMsg.front().getData(), m_wirteMsg.front().getLen()),
-            boost::bind(&chatSession::handler_write, shared_from_this(), boost::asio::placeholders::error));
-
-        m_wirteMsg.pop_front(); //写完一个消息则弹出
+        std::cerr << "解码失败..." << std::endl;
+//        m_pRoom.leave()
+        return;
     }
+
+    boost::asio::async_read(m_sock, buffer(m_readingChatMsg.body(), m_readingChatMsg.bodyLen()),
+                            boost::bind(&chatSession::handler_readbody, shared_from_this(), placeholders::error));
+}
+
+void chatSession::handler_readbody(const error_code_type &ec)
+{
+    if(ec)
+    {
+        std::cerr << "读取消息体数据失败" << std::endl;
+        return;
+    }
+
+    std::cout << "reback client msg: " << m_readingChatMsg.body() << std::endl;
+    memset(m_readingChatMsg.getData(), 0, HEADER_LEN + BODY_LEN);
+
+    boost::asio::async_read(m_sock, buffer(m_readingChatMsg.getData(), HEADER_LEN),
+                            boost::bind(&chatSession::handler_readbody, shared_from_this(), placeholders::error));
 }
 
 void chatSession::deliver(chatMsg& msg)
@@ -45,12 +54,22 @@ void chatSession::deliver(chatMsg& msg)
     {
         boost::asio::async_write(m_sock, buffer(m_wirteMsg.front().getData(), m_wirteMsg.front().getLen()),
             boost::bind(&chatSession::handler_write, shared_from_this(), boost::asio::placeholders::error));
-
-        m_wirteMsg.pop_front();
     }
     else
     {
         m_pRoom.leave(shared_from_this());
     }
 
+}
+
+void chatSession::handler_write(const error_code_type &ec)
+{
+    if(ec)
+    {
+        std::cerr << "发送数据错误..." << std::endl;
+        return;
+    }
+    m_wirteMsg.pop_front();
+    boost::asio::async_write(m_sock, buffer(m_wirteMsg.front().body(), m_wirteMsg.front().bodyLen()),
+                             boost::bind(&chatSession::handler_write, shared_from_this(), placeholders::error));
 }
--- CppExcise/boostExcise/boost_asio/chat_project/server/chatSession.h
+++ CppExcise/boostExcise/boost_asio/chat_project/server/chatSession.h
@@ -13,35 +13,31 @@
 class chatRoom;
 using namespace boost::asio;
 
-class chatParticipant
-{
-public:
-    virtual ~chatParticipant() {}
-
-    virtual void deliver(chatMsg& msg) = 0;
-};
 
 class chatSession : public chatParticipant, public boost::enable_shared_from_this<chatSession>
 {
 private:
-    chatMsg m_chatMsg;
-    ip::tcp::socket& m_sock;
+    chatMsg m_readingChatMsg; //正在读的消息缓存
+    ip::tcp::socket m_sock;
     chatRoom& m_pRoom;
-    std::deque<chatMsg> m_wirteMsg;
+    std::deque<chatMsg> m_wirteMsg; //正在写的消息队列
 public:
-    chatSession(boost::asio::io_service& ioserver, chatRoom& room) : m_sock(ioserver), m_pRoom(room) {}
-    ~chatSession();
+    chatSession(io_service& ioserver, chatRoom& room) : m_sock(ioserver), m_pRoom(room) {}
+    ~chatSession() {}
 
     void deliver(chatMsg& msg);
+
+    void start();
+
 
     ip::tcp::socket& sock(){
         return m_sock;
     }
 
-    void start();
-    void handler_read(boost::system::error_code ec);
-    void handler_write(boost::system::error_code ec);
-    
+    void handler_readhead(const error_code_type& ec);
+    void handler_readbody(const error_code_type& ec);
+    void handler_write(const error_code_type& ec);
+
 };
 
 
