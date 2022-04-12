2095f6b993f11caa389fd2bd0b09c496a4eca8bb
wenheng1644 <wenheng1644@gmail.com>
2022-04-13 00:26:51 +0800

    测试1

--- CppExcise/boostExcise/boost_asio/chat_project/server/chatRoom.cpp
+++ CppExcise/boostExcise/boost_asio/chat_project/server/chatRoom.cpp
@@ -9,7 +9,7 @@ void chatRoom::join(participant_ptr particiant)
         return;
 
     m_sessions.insert(particiant);
-    std::cout << "pepole coming...\t" << m_sessions.size() << std::endl;
+//    std::cout << "pepole coming...\t" << m_sessions.size() << std::endl;
 
     std::for_each(m_chatMsgs.begin(), m_chatMsgs.end(), boost::bind(&chatParticipant::deliver, particiant, boost::placeholders::_1));
 }
@@ -22,14 +22,14 @@ void chatRoom::leave(participant_ptr particiant)
     if(!m_sessions.count(particiant))
         return;
 
-    std::cout << "pepole leaving...\t" << m_sessions.size() << std::endl;
+//    std::cout << "pepole leaving...\t" << m_sessions.size() << std::endl;
     m_sessions.erase(particiant);
 }
 
 
 void chatRoom::deliver(chatMsg& msg)
 {
-    std::cout << "pepoles: " << m_sessions.size() << std::endl;
+//    std::cout << "pepoles: " << m_sessions.size() << std::endl;
     if(m_sessions.empty())
         return;
     m_chatMsgs.push_back(msg);
