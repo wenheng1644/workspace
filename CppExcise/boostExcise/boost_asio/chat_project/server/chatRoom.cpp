334191e13a73c6f435368785a37ffc4c622baf0d
wenheng1644 <wenheng1644@gmail.com>
2022-04-13 00:24:55 +0800

    测试1

--- CppExcise/boostExcise/boost_asio/chat_project/server/chatRoom.cpp
+++ CppExcise/boostExcise/boost_asio/chat_project/server/chatRoom.cpp
@@ -1,13 +1,15 @@
 #include "chatRoom.h"
 #include "boost/bind/bind.hpp"
 
+#include "iostream"
+
 void chatRoom::join(participant_ptr particiant)
 {
     if(m_sessions.count(particiant))
         return;
 
     m_sessions.insert(particiant);
-
+    std::cout << "pepole coming...\t" << m_sessions.size() << std::endl;
 
     std::for_each(m_chatMsgs.begin(), m_chatMsgs.end(), boost::bind(&chatParticipant::deliver, particiant, boost::placeholders::_1));
 }
@@ -20,14 +22,16 @@ void chatRoom::leave(participant_ptr particiant)
     if(!m_sessions.count(particiant))
         return;
 
+    std::cout << "pepole leaving...\t" << m_sessions.size() << std::endl;
     m_sessions.erase(particiant);
 }
 
 
 void chatRoom::deliver(chatMsg& msg)
 {
+    std::cout << "pepoles: " << m_sessions.size() << std::endl;
     if(m_sessions.empty())
         return;
-    
+    m_chatMsgs.push_back(msg);
     std::for_each(m_sessions.begin(), m_sessions.end(), std::bind(&chatParticipant::deliver, std::placeholders::_1, msg));
 }
--- CppExcise/boostExcise/boost_asio/chat_project/server/chatSession.cpp
+++ CppExcise/boostExcise/boost_asio/chat_project/server/chatSession.cpp
@@ -6,7 +6,8 @@
 void chatSession::start()
 {
     m_pRoom.join(shared_from_this());
-
+    memset(m_readingChatMsg.getData(), 0, HEADER_LEN + BODY_LEN);
+    std::cout << "[connect] remote ip: " << m_sock.remote_endpoint().address().to_string() << std::endl;
     boost::asio::async_read(m_sock, buffer(m_readingChatMsg.getData(), HEADER_LEN), boost::bind(&chatSession::handler_readhead, shared_from_this(), boost::asio::placeholders::error));
 }
 
@@ -22,7 +23,7 @@ void chatSession::handler_readhead(const error_code_type &ec)
     if(!m_readingChatMsg.decode())
     {
         std::cerr << "解码失败..." << std::endl;
-//        m_pRoom.leave()
+        m_pRoom.leave(shared_from_this());
         return;
     }
 
@@ -38,11 +39,25 @@ void chatSession::handler_readbody(const error_code_type &ec)
         return;
     }
 
-    std::cout << "reback client msg: " << m_readingChatMsg.body() << std::endl;
+//    std::cout << "" <<"reback client msg: " << m_readingChatMsg.body() << std::endl;
+    printf("[%s]:\t%s\n", m_sock.remote_endpoint().address().to_string().data(), m_readingChatMsg.body());
+    char msg[BODY_LEN + 1] = "";
+    memset(msg, 0, BODY_LEN + 1);
+
+    sprintf(msg,"[%s]:\t%s\n", m_sock.remote_endpoint().address().to_string().data(), m_readingChatMsg.body());
+    //*******************************************************
+    chatMsg newChatMsg;
+    memset(newChatMsg.body(), 0, HEADER_LEN + BODY_LEN);
+    memcpy(newChatMsg.body(), msg, strlen(msg));
+    newChatMsg.bodyLen(strlen(msg));
+    newChatMsg.encode();
+//    *******************************************************
+
+    m_pRoom.deliver(newChatMsg); //将消息分发给所有客户端
     memset(m_readingChatMsg.getData(), 0, HEADER_LEN + BODY_LEN);
 
     boost::asio::async_read(m_sock, buffer(m_readingChatMsg.getData(), HEADER_LEN),
-                            boost::bind(&chatSession::handler_readbody, shared_from_this(), placeholders::error));
+                            boost::bind(&chatSession::handler_readhead, shared_from_this(), placeholders::error));
 }
 
 void chatSession::deliver(chatMsg& msg)
@@ -55,10 +70,6 @@ void chatSession::deliver(chatMsg& msg)
         boost::asio::async_write(m_sock, buffer(m_wirteMsg.front().getData(), m_wirteMsg.front().getLen()),
             boost::bind(&chatSession::handler_write, shared_from_this(), boost::asio::placeholders::error));
     }
-    else
-    {
-        m_pRoom.leave(shared_from_this());
-    }
 
 }
 
@@ -66,10 +77,16 @@ void chatSession::handler_write(const error_code_type &ec)
 {
     if(ec)
     {
-        std::cerr << "发送数据错误..." << std::endl;
+//        std::cerr << "send data error..." << std::endl;
         return;
     }
     m_wirteMsg.pop_front();
-    boost::asio::async_write(m_sock, buffer(m_wirteMsg.front().body(), m_wirteMsg.front().bodyLen()),
+    if(m_wirteMsg.empty())
+    {
+//        std::cerr << "datas empty..." << std::endl;
+        return;
+    }
+
+    boost::asio::async_write(m_sock, buffer(m_wirteMsg.front().getData(), m_wirteMsg.front().getLen()),
                              boost::bind(&chatSession::handler_write, shared_from_this(), placeholders::error));
 }
