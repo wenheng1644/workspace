f81c81ca4b89ba6a82a6b596beec5e6ce1a8faf9
wenheng1644 <wenheng1644@gmail.com>
2023-03-18 17:16:00 +0800

    测试提交

--- CAndLua/luaCall/luaScript/luaMain.lua
+++ CAndLua/luaCall/luaScript/luaMain.lua
@@ -4,12 +4,14 @@ local prefixPath = "/home/wenheng/Desktop/workspace/CAndLua/luaCall/extra"
 local cLUAPATH = ";" .. prefixPath .. "/cLua/lib/lib?.so"
 local transPATH = ";" .. prefixPath .. "/transLib/lib/lib?.so"
 local uptransPATH = ";" .. prefixPath .. "/uptransLib/lib/lib?.so"
-package.cpath = package.cpath ..cLUAPATH .. transPATH .. uptransPATH
+local arrayPATH = ";" .. prefixPath .. "/arrayLib/lib/lib?.so"
+package.cpath = package.cpath ..cLUAPATH .. transPATH .. uptransPATH .. arrayPATH
 package.loaded["translib"] = nil
 package.loaded["mylib"] = nil
 local mylib = require("mylib")
 local translib = require("translib")
 local uptranslib = require("uptranslib")
+local arraylib = require("arraylib")
 local function test_dirs(path)
     local dirs = c_dirs2(path or ".")
 
@@ -55,17 +57,29 @@ function main()
     --    print(k, v)
     --end
 
-    uptranslib.upSettrans(tb)
-    local tb2 = uptranslib.upGettrans()
-    if not tb2 then
-        print("tb2 is nil")
-    elseif not next(tb2) then
-        print("tb2 is no data")
-    end
-    for k, v in pairs(tb2) do
-        print(k, v)
-    end
-    print(uptranslib.upTransliterate(str))
+    --uptranslib.upSettrans(tb)
+    --local tb2 = uptranslib.upGettrans()
+    --if not tb2 then
+    --    print("tb2 is nil")
+    --elseif not next(tb2) then
+    --    print("tb2 is no data")
+    --end
+    --for k, v in pairs(tb2) do
+    --    print(k, v)
+    --end
+    --print(uptranslib.upTransliterate(str))
+
+    --for k, v in pairs(arraylib) do
+    --    print(k, v)
+    --end
+    local a = arraylib.new(1000)
+    local b = arraylib.new(1000)
+    a[1000] = true
+    --b[1000] = true
+    print("a[1000], b[1000] = ", a[1000], b[1000])
+    local c = a | b --测试或运算
+    print("c = ", c)
+    print("c[1000] = ", c[1000])
 end
 
 main()
